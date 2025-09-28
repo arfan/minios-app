ca71ceff1fe71700d81dba4f2fab1d20c1cfebf2
abdul.arfan <abdul.arfan@gmail.com>
2025-09-28 21:27:04 +0700

    remove everything

--- colorapp/script.js
+++
@@ -1,7 +0,0 @@
-const colors = ["#ff6b6b", "#6bc1ff", "#6bff95", "#ffe16b", "#b76bff"];
-let index = 0;
-
-function changeColor() {
-  document.body.style.backgroundColor = colors[index];
-  index = (index + 1) % colors.length;
-}
